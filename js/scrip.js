1e09d22f896d33853b44582df52f4dfed2bfc4ae
João Paulo <103288654+Jounys@users.noreply.github.com>
2022-06-03 22:36:55 -0300

    Initial commit

---
+++ js/scrip.js
@@ -0,0 +1,49 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => { 
+        mario.classList.remove('jump');
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    console.log(marioPosition);
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = 'imagens/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
+
+    }
+
+}, 10);
+
+let score = document.querySelector("score");
+
+let Interval = null;
+let playerScore = 0;
+
+function scoreCounter() {
+    playerScore++;
+    score.innerHTML = `Score <b>${playerScore}</b>`;
+}
+
+Interval = setInterval(scoreCounter, 200)
+
+document.addEventListener('keydown', jump)
